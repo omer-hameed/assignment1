a3ef955cce1df2aebf6be1d2e4f331c694322f21
Umer <omerhamid.255@gmail.com>
2024-07-03 14:50:04 +0500

    Complete Files

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// DAY 1
+// Question # 1 Define a function named multiply that takes two numbers as parameters and returns their product. Write code to test this function with different input values.
+
+function Multiply(x, y) {
+  return x * y;
+}
+const product = Multiply(3, 8);
+
+console.log(product);
